034860020438afef38d2f3d6ea75360fc59765b1
Rhainte <king8557@naver.com>
2022-07-22 11:13:29 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Holiday from './holiday';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <Holiday />
   </React.StrictMode>
 );
 
